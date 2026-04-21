ab09b31252c22ec9746b0af33e10d55577d29e97
olungatravor <trevorlovicks@gmail.com>
2026-04-21 19:07:26 +0300

    cgpa

--- main.c
+++ main.c
@@ -40,9 +40,9 @@ int main(void) {
 
     /* Validate input */
     for(int i = 0; i < 8; i++) {
-        if(s1[i] < 0 || s1[i] > 100 ||s1[i] < 0 || s2[i] > 100) {
+        if(s1[i] < 0 || s1[i] > 100 ||s2[i] < 0 || s2[i] > 100) {
             printf("Invalid score entered\n");
-            return 1;
+            return 0;
         }
     }
 
